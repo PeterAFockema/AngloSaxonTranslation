560b57885b5761f564cb79cbdd1189da808333ee
PeterAFockema <anonymous@person.com>
2025-12-12 18:48:24 +0000

    Updated the install_requires in setup.py.

--- setup.py
+++ setup.py
@@ -5,4 +5,5 @@ setuptools.setup(
         version="0.0.1",
         python_requires=">=3.6",
         packages=["anglosaxon"],
+        install_requires=['torch','torch.nn','torch','torch.nn.functional','numpy','torch.utils.data']
         )
