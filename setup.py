585129e2ab3e67de2f51d7f7e51c3b2e06c57aa1
PeterAFockema <anonymous@person.com>
2025-12-17 08:58:32 +0000

    Changing directory structure

--- setup.py
+++
@@ -1,9 +0,0 @@
-import setuptools
-
-setuptools.setup(
-        name="sub-module",
-        version="0.0.1",
-        python_requires=">=3.6",
-        packages=["anglosaxon"],
-        install_requires=['torch','torch.nn','torch','torch.nn.functional','numpy','torch.utils.data']
-        )
