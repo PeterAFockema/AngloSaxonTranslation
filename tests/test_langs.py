f7801d9de4adc882632cb526140ee982386c11bc
PeterAFockema <anonymous@person.com>
2025-12-21 16:36:54 +0000

    Added langs test

---
+++ tests/test_langs.py
@@ -0,0 +1,11 @@
+import unittest
+
+from anglosaxon.langs import return_Langs_object
+
+class TestLangs(unittest.TestCase):
+
+    def test_return_Langs_object(self):
+        return_Langs_object("test")
+
+if __name__ == '__main__':
+    unittest.main()
