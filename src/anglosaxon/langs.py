734d6fa46fe1493f553e31b7369a99c2f1844908
PeterAFockema <anonymous@person.com>
2025-12-26 09:43:13 +0000

    Updated imports for Lang

--- src/anglosaxon/langs.py
+++ src/anglosaxon/langs.py
@@ -1,4 +1,4 @@
-from AngloSaxonTranslation.src.anglosaxon.language_helpers import anglosaxon_lang
+from anglosaxon.language_helpers import anglosaxon_lang
 
 def return_Langs_object(self, name:str):
     return anglosaxon_lang.Lang(name)
