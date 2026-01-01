11615d1c53b3bada75b247d6578e838fe049c711
PeterAFockema <anonymous@person.com>
2026-01-01 14:47:19 +0000

    Adding preliminary EncoderRNN class architecture.

---
+++ tests/encoders/test_encoder_rnn.py
@@ -0,0 +1,11 @@
+import unittest
+
+from anglosaxon.encoders.encoder_rnn import EncoderRNN
+
+class TestLangs(unittest.TestCase):
+
+    def test_EncoderRNN(self):
+        EncoderRNN(self, "input_lang".n_words, 5)
+
+if __name__ == '__main__':
+    unittest.main()
